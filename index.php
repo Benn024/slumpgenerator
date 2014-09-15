9c10c8e0019764307906411bc734e111e776369d
marben431 <marben431@E29613.edu.linkoping.se>
2014-09-15 11:00:26 +0200

    vi suger, det går skit, men ndå framåt, Löfven kommer bli vårat fall. GG Sverige

--- index.php
+++ index.php
@@ -10,8 +10,22 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+<!--        <form>
+            <input type="text" name="namn">
+            <input type="submit"> 
+        </form>-->
         <?php
-        // put your code here
+        
+//        var_dump($_GET);
+//        echo $_GET["namn"];
+        
+        $namn = array("martin","albin","klas","carl","fgt","prinsessukennyruuuuuuuuu","mr cool","apskaft","fett sne","rebecka");
+        $sak = rand(0,9);
+        
+        echo "Det var en solig dag och ".$namn[$sak]." åt glass";
+        
+        
+        
         ?>
     </body>
 </html>
